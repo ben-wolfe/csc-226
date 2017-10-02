6ee16110fc6e719a51d3053799798d80a15ddc25
Ben Wolfe <wolfeben16@gmail.com>
2017-10-02 01:25:45 -0700

    Buggy out of bounds code

--- A1/QuickSelect.java
+++ A1/QuickSelect.java
@@ -8,17 +8,90 @@
 
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 public class QuickSelect {
        
     public static int QuickSelect(int[] A, int k){
-    	int[] noDuplicates = RemoveDuplicates(A);
 
-        return 0;
+    	if (A.length == 0) return -1;  // Check for a valid array
+
+    	int[] noDuplicates = RemoveDuplicates(A);  // Remove duplicates    	
+    	if (noDuplicates.length < k) return -1;
+
+    	ArrayList<Integer> arrayList = new ArrayList<>(A.length);
+    	for (int num : A) {
+    		arrayList.add(Integer.valueOf(num));
+    	}
+
+    	int kthValue = medianOfMedians(arrayList, k);  // Find the kth order statistic */
+        return kthValue;
     }   
 
-    public static int[] RemoveDuplicates(int[] A) {
+    public static int medianOfMedians(ArrayList<Integer> A, int k) {
+
+    	for (Integer num : A) {
+    		System.out.print(num + ", ");
+    	}
+    	System.out.println();
+
+    	int kthValue;
+    	int pivot;
+
+    	/* Problem 1 is getting out of bounds erros here.  Notice how size = 3 and 3 % 7 = 4; 3-4 = -1 so the 
+    	   for loop never executes */
+    	ArrayList<Integer> allMedians = new ArrayList<Integer>();
+    	System.out.println(A.size());
+    	for(int index=0; index < (A.size() - A.size() % 7); index = index+7) {
+			ArrayList<Integer> subList = new ArrayList<Integer>(A.subList(index, index+7));
+
+			System.out.print("Sublist: ");
+	    	for (Integer num : subList) {
+	    		System.out.print(num + ", ");
+	    	}
+	    	System.out.println();
+
+    		allMedians.add(findMedian(subList));    		
+    	}
+
+    	if (allMedians.size() <= 7) {
+    		Collections.sort(allMedians);
+    		pivot = allMedians.get(allMedians.size()/2);
+
+    	} else {
+    		pivot = medianOfMedians(allMedians, allMedians.size()/2);
+    	}
+
+
+    	ArrayList<Integer> low = new ArrayList<Integer>();
+    	ArrayList<Integer> high = new ArrayList<Integer>();
     	
+    	for (Integer num : A) {
+    		if (num < pivot) low.add(num);
+    		else if (num > pivot) high.add(num);
+    	}
+
+    	int lowLength = low.size();
+    	if (k < lowLength) {
+    		return medianOfMedians(low, k);
+    	} else if (k == lowLength+1) {
+    		return pivot;
+    	} else {
+    		return medianOfMedians(high, k-lowLength-1);
+    	}	
+    }
+
+    public static int findMedian(ArrayList<Integer> nums) {
+    	Collections.sort(nums);
+    	return nums.get(nums.size()/2);
+    }
+
+    public static int[] RemoveDuplicates(int[] A) {
+    /* This function removes duplicates by attempting to add every element to a hash set.  The hash set class implements 
+       the set interface backed by a hash table meaning that no duplicate entries are allowed.  Time complexity is O(n) to 
+       iterate over the entire array.
+       Reference: https://docs.oracle.com/javase/7/docs/api/java/util/HashSet.html
+	*/
     	HashSet<Integer> hashSet = new HashSet<Integer>(); 
     	for (int num : A) {
     		hashSet.add(num);
@@ -35,7 +108,9 @@ public class QuickSelect {
     }
     
     public static void main(String[] args) {
-        int[] A = {50, 54, 49, 49, 48, 49, 56, 52, 51, 52, 50, 59};
-        System.out.println("The median is " + QuickSelect(A, (A.length+1)/2));
+        int[] A = {1, 2, 3, 4, 5, 6, 7};
+       //System.out.println("The kth smallest value is " + QuickSelect(A, (A.length+1)/2));
+        System.out.println("The kth smallest value is " + QuickSelect(A, 2));
+
     }
 }
