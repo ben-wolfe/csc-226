72ab1280c79c2ca1858e4c120c04d31773fc0f2c
Ben Wolfe <wolfeben16@gmail.com>
2017-10-01 20:50:42 -0700

    Implement remove duplicates revision 2

--- A1/QuickSelect.java
+++ A1/QuickSelect.java
@@ -12,44 +12,30 @@ import java.io.*;
 public class QuickSelect {
        
     public static int QuickSelect(int[] A, int k){
-     	
-    	int kthOrder = 0;
+    	int[] noDuplicates = RemoveDuplicates(A);
 
-    	int[] noDuplicates = removeDuplicates(A);
-    	for (int num : noDuplicates) {
-    		System.out.print(num + " ");
-    	}
-    	
-        return kthOrder;
+        return 0;
     }   
 
-    public static int[] removeDuplicates(int[] A) {
-    /* This function takes an array and returns a duplicate-free version.  The worst case running time
-       is O(n) as a result of traversing over all elements of the array to create the mapping and also 
-       to convert back to an array */
-
-       	// Create an Integer list to be able to apply hash set
-		List<Integer> integerList = new ArrayList<Integer>();
-		for (int index = 0; index < A.length; index++) {
-			integerList.add(A[index]);
-		}
-
-		// Make a set to remove duplicates
-    	Set<Integer> arraySet = new HashSet<Integer>(integerList);
+    public static int[] RemoveDuplicates(int[] A) {
     	
-    	// Convert back to int[]
-    	int[] noDuplicates = new int[arraySet.size()];
-    	int index = 0;
-    	for ( Integer i : arraySet) {
-    		noDuplicates[index++] = i;
-    	}
+    	HashSet<Integer> hashSet = new HashSet<Integer>(); 
+    	for (int num : A) {
+    		hashSet.add(num);
+    	} 
+		
+		Iterator<Integer> itr=hashSet.iterator();
+		int[] noDuplicates = new int[hashSet.size()];
+		int index = 0;  
+ 		while(itr.hasNext()){  
+ 			noDuplicates[index++] = itr.next();
+  		} 
 
-    	return noDuplicates;
-    } 
+  		return noDuplicates;
+    }
     
     public static void main(String[] args) {
         int[] A = {50, 54, 49, 49, 48, 49, 56, 52, 51, 52, 50, 59};
-
         System.out.println("The median is " + QuickSelect(A, (A.length+1)/2));
     }
 }
