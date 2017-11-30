41e1411a21cffaba538f5b02ef203a3b596e2cf4
Ben Wolfe <wolfeben16@gmail.com>
2017-11-30 15:45:49 -0800

    Working implementation of KMP with comments

--- A4/KMP.java
+++ A4/KMP.java
@@ -1,18 +1,76 @@
+/* 
+	Submited by:
+		Ben Wolfe | V00205547
+
+	Course:
+		CSC 226 - Algorithms and Data Structures II
+
+	Deliverable:
+		Assignment 4 - KMP String Search
+
+	Description:
+		This assignments generates the deterministic finite automon for a
+		given pattern.  It then searches a provided text file for this pattern
+		using the DFA.  The implementation is based off the example in our 
+		textbook: Algorithms by Robert Sedgewick and Kevin Wayne.
+*/
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 public class  KMP {
 
     private static String pattern;
+    private static int[][] dfa;      
+    private final int alphabet; 
 
     public KMP(String pattern){  
 
+        this.alphabet = 256;                 // This is the maximum character code for ASCII character set
+                                             // It's more advantageous to set up the program using this general
+                                             // configuration since it does not interfer with the asymptotic
+                                             // running time and makes the program more versatile since we can
+                                             // just use the numeric value of each character at each step.
+
+        this.pattern = pattern;
+        int patternLength = pattern.length();
+
+        // Initialize the state transition array
+        dfa = new int[alphabet][patternLength]; 
+
+        // Initialize the first column (state 0) of the array
+        dfa[pattern.charAt(0)][0] = 1; 
+
+        // Iterate through each character of the pattern
+        for (int refState=0, currState=1; currState < patternLength; currState++) {
+
+        	// For each character, populate the alphabet column using the reference state
+            for (int pattChar = 0; pattChar < alphabet; pattChar++)
+            	dfa[pattChar][currState] = dfa[pattChar][refState];
+            
+            // Update state to correspond with a matched character
+            dfa[pattern.charAt(currState)][currState] = currState+1;
+
+            // Update the reference state for the next iteration
+            // This acts as considering a substring from [1,...,n]
+            refState = dfa[pattern.charAt(currState)][refState];
+        }
     }
     
     public static int search(String txt) {  
-		return 0;
+		
+        int patternLength = pattern.length();
+        int textLength = txt.length();
+
+        // For each character in the text, bounce around the state machine
+        // If the state reaches the final state, then return match
+        int i, j;
+        for (i = 0, j = 0; i < textLength && j < patternLength; i++) {
+            j = dfa[txt.charAt(i)][j];
+        }
+
+        return (j == patternLength) ? i-patternLength : textLength;
     }
     
     
@@ -20,34 +78,29 @@ public class  KMP {
 		
 		Scanner s;
 		if (args.length > 0){
-	    try{
-			s = new Scanner(new File(args[0]));
-	    } catch(java.io.FileNotFoundException e){
-			System.out.println("Unable to open "+args[0]+ ".");
-			return;
-	    }
-	    
-	    System.out.println("Opened file "+args[0] + ".");
-	    
-	    String text = "";
-	    while(s.hasNext()){
-			text += s.next() + " ";
-	    }
-	    
-	    for(int i = 1; i < args.length; i++){
-			KMP k = new KMP(args[i]);
-			int index = search(text);
-			if(index >= text.length())System.out.println(args[i] + " was not found.");
-			else System.out.println("The string \"" + args[i] + "\" was found at index " + index + ".");
-	    }
-	    
-	    //System.out.println(text);
-	    
-		}
+		    try{
+				s = new Scanner(new File(args[0]));
+		    } catch(java.io.FileNotFoundException e){
+				System.out.println("Unable to open "+args[0]+ ".");
+				return;
+		    }
+		    
+		    System.out.println("Opened file "+args[0] + ".");
+		    
+		    String text = "";
+		    while(s.hasNext()){
+				text += s.next() + " ";
+		    }
+		    
+		    for(int i = 1; i < args.length; i++){
+				KMP k = new KMP(args[i]);
+				int index = search(text);
+				if(index >= text.length())System.out.println(args[i] + " was not found.");
+				else System.out.println("The string \"" + args[i] + "\" was found at index " + index + ".");
+		    }
+		} 
 		else {
-	    	System.out.println("usage: java SubstringSearch <filename> <pattern_1> <pattern_2> ... <pattern_n>.");
+	    	System.out.println("usage: java KMP <filename> <pattern_1> <pattern_2> ... <pattern_n>.");
 		}
-	
-	
     }
 }
